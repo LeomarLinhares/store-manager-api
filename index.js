dced28a0f66d3cdc81917176b1c146bc63edde54
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-27 12:42:05 -0300

    :construction: adiciona os middlewares do método post da rota products

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const productsController = require('./controllers/productsController');
+const {
+  validateName,
+  validateIfProductExists,
+  validateQuantity,
+} = require('./middlewares/validateProducts');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,8 +16,8 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-app.route('/products')
-  .post(productsController.create);
+app.route('/products/')
+  .post(validateName, validateIfProductExists, validateQuantity, productsController.create);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
