54bbb61a71502be071470bd04d3d60e312483322
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-27 11:07:53 -0300

    :sparkles: cria objeto contendo mensagens que a API vai utilizar

---
+++ helpers/messages.js
@@ -0,0 +1,6 @@
+module.exports = {
+  NAME_REQUIRED: '"name" is required',
+  NAME_5_CHARACTERS: '"name" length must be at least 5 characters long',
+  PRODUCT_ALREADY_EXISTS: 'Product already exists',
+  DATABASE_ERROR_CREATE: 'Ocorreu um erro ao tentar cadastrar produto ao bando de dados',
+};
