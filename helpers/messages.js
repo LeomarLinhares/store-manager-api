fa219fe9279f4714b2a6baf08bb96e66a7698e83
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-27 17:23:52 -0300

    :pencil: deixa todas as mensagens em inglês

--- helpers/messages.js
+++ helpers/messages.js
@@ -2,7 +2,7 @@ module.exports = {
   NAME_REQUIRED: '"name" is required',
   NAME_5_CHARACTERS: '"name" length must be at least 5 characters long',
   PRODUCT_ALREADY_EXISTS: 'Product already exists',
-  DATABASE_ERROR_CREATE: 'Ocorreu um erro ao tentar cadastrar produto ao bando de dados',
+  DATABASE_ERROR_CREATE: 'An error occurred when trying to add product to the database',
   QUANTITY_REQUIRED: '"quantity" is required',
   QUANTITY_LARGER_THAN_0: '"quantity" must be a number larger than or equal to 1',
 };
