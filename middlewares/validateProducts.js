0a9271a3694b84f8c938e4a40f3465c3e3bed711
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-27 12:40:42 -0300

    :sparkles: cria middleware que valida se o produto já existe no database

--- middlewares/validateProducts.js
+++ middlewares/validateProducts.js
@@ -1,10 +1,20 @@
 const msg = require('../helpers/messages');
+const productsModel = require('../models/productsModel');
 
 module.exports = {
   validateName: (req, res, next) => {
     const { name } = req.body;
     if (!name) return res.status(400).json({ message: msg.NAME_REQUIRED });
     if (name.length < 5) return res.status(422).json({ message: msg.NAME_5_CHARACTERS });
+
+    next();
+  },
+
+  validateIfProductExists: async (req, res, next) => {
+    const { name } = req.body;
+    const products = await productsModel.getAll();
+    const alreadyExists = products.some((product) => product.name === name);
+    if (alreadyExists) return res.status(409).json({ message: msg.PRODUCT_ALREADY_EXISTS });
 
     next();
   },
