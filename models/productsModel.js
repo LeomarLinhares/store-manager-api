67d9ebbaebfc7cd1d492a131a595a336527b0671
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-27 13:42:05 -0300

    :contruction: refaz o productsModel declarando explicitamente o banco de dados

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,19 +1,19 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [rows] = await connection.execute('SELECT * FROM products');
+  const [rows] = await connection.execute('SELECT * FROM StoreManager.products');
   return rows;
 };
 
 const getById = async (id) => {
-  const [row] = await connection.execute('SELECT * FROM products WHERE id = ?', [id]);
+  const [row] = await connection.execute('SELECT * FROM StoreManager.products WHERE id = ?', [id]);
   const product = row[0];
   return product;
 };
 
 const create = async (name, quantity) => {
   const [rows] = await connection.execute(
-    'INSERT INTO products (name, quantity) VALUES (?, ?)',
+    'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?)',
     [name, quantity],
   );
 
@@ -26,7 +26,7 @@ const create = async (name, quantity) => {
 
 const update = async (name, quantity, id) => {
   await connection.execute(`
-    UPDATE products
+    UPDATE StoreManager.products
       SET name = ?, quantity = ?
     WHERE id = ?
   `, [name, quantity, id]);
@@ -34,7 +34,7 @@ const update = async (name, quantity, id) => {
 };
 
 const remove = async (id) => {
-  await connection.execute('DELETE FROM products WHERE id = ?', [id]);
+  await connection.execute('DELETE FROM StoreManager.products WHERE id = ?', [id]);
 };
 
 module.exports = {
