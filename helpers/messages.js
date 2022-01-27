eef8a50febf667986500682900e5d322001f666f
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-27 12:39:04 -0300

    :construction: adiciona novas mensagens ao messages.js

--- helpers/messages.js
+++ helpers/messages.js
@@ -3,4 +3,6 @@ module.exports = {
   NAME_5_CHARACTERS: '"name" length must be at least 5 characters long',
   PRODUCT_ALREADY_EXISTS: 'Product already exists',
   DATABASE_ERROR_CREATE: 'Ocorreu um erro ao tentar cadastrar produto ao bando de dados',
+  QUANTITY_REQUIRED: '"quantity" is required',
+  QUANTITY_LARGER_THAN_0: '"quantity" must be a number larger than or equal to 1',
 };
