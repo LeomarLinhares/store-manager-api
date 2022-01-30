6df74f7a098fd6bd92c073b2ec746b8f3bf06e63
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-30 19:48:58 -0300

    :bug: corrige o response da API quando uma venda era efetuada com sucesso

--- services/salesService.js
+++ services/salesService.js
@@ -7,7 +7,11 @@ module.exports = {
       const { insertId } = await salesModel.create();
       const allSales = sales.map((product) => [insertId, product.product_id, product.quantity]);
       console.log(allSales);
-      return await salesProductsModel.create(allSales);
+      await salesProductsModel.create(allSales);
+      return {
+        id: insertId,
+        itemsSold: sales,
+      };
     } catch (error) {
       console.log(error);
     }
