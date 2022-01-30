a7fa30c86329dba0835bebf58ea12b3003e48e89
Leomar Linhares <leomar_linhares@hotmail.com>
2022-01-30 18:51:59 -0300

    :construction: adiciona queries relacionadas a rota sales

--- helpers/queries.js
+++ helpers/queries.js
@@ -1,7 +1,12 @@
 module.exports = {
-  SELECT_ALL: 'SELECT * FROM StoreManager.products',
-  SELECT_BY_ID: 'SELECT * FROM StoreManager.products WHERE id = ?',
+  SELECT_ALL_PRODUCTS: 'SELECT * FROM StoreManager.products',
+  SELECT_PRODUCTS_BY_ID: 'SELECT * FROM StoreManager.products WHERE id = ?',
   INSERT_PRODUCT: 'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?)',
   UPDATE_PRODUCT: 'UPDATE StoreManager.products SET name = ?, quantity = ? WHERE id = ?',
   REMOVE_PRODUCT: 'DELETE FROM StoreManager.products WHERE id = ?',
+
+  INSERT_SALE: 'INSERT INTO StoreManager.sales (`date`) VALUE (CURRENT_TIMESTAMP)',
+
+  INSERT_SALES_PRODUCTS: `INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity)
+  VALUES ?`,
 };
