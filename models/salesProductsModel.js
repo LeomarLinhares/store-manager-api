605bdbf81e1787e522021135f2356a7d65c4a1db
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-02 19:36:50 -0300

    :sparkles: cria o model getAll para a rota /sales no salesProductsModel

--- models/salesProductsModel.js
+++ models/salesProductsModel.js
@@ -1,11 +1,14 @@
 const connection = require('./connection');
 const QUERY = require('../helpers/queries');
 
-const create = async (allSales) => {
-  const [rows] = await connection.query(QUERY.INSERT_SALES_PRODUCTS, [allSales]);
-  return rows;
-};
-
 module.exports = {
-  create,
+  create: async (allSales) => {
+    const [rows] = await connection.query(QUERY.INSERT_SALES_PRODUCTS, [allSales]);
+    return rows;
+  },
+
+  getAll: async () => {
+    const [rows] = await connection.execute(QUERY.SELECT_ALL_SALES_PRODUCTS);
+    return rows;
+  },
 };
