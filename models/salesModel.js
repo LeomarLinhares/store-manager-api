66470631e0e59dfa7e5518409740839c9428eda5
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-09 10:08:26 -0300

    :recycle: refatora a estrutura do módulo salesModel.js para que os testes enxerguem as funções

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,19 +1,23 @@
 const connection = require('./connection');
 const QUERY = require('../helpers/queries');
 
+const create = async () => {
+  const [rows] = await connection.execute(QUERY.INSERT_SALE);
+  return rows;
+};
+
+const getAll = async () => {
+  const [rows] = await connection.execute(QUERY.SELECT_ALL_SALES);
+  return rows;
+};
+
+const getById = async (id) => {
+  const [rows] = await connection.execute(QUERY.SELECT_SALE_BY_ID, [id]);
+  return rows;
+};
+
 module.exports = {
-  create: async () => {
-    const [rows] = await connection.execute(QUERY.INSERT_SALE);
-    return rows;
-  },
-
-  getAll: async () => {
-    const [rows] = await connection.execute(QUERY.SELECT_ALL_SALES);
-    return rows;
-  },
-
-  getById: async (id) => {
-    const [rows] = await connection.execute(QUERY.SELECT_SALE_BY_ID, [id]);
-    return rows;
-  },
+  create,
+  getAll,
+  getById,
 };
