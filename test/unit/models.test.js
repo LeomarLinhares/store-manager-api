f3e76c7eb795554ba2231e9a36213b8640e617b2
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-09 09:50:31 -0300

    :test_tube: testa a função create do productsModel

--- test/unit/models.test.js
+++ test/unit/models.test.js
@@ -34,4 +34,29 @@ describe('O model productsModel', () => {
       expect(result).to.have.lengthOf(1);
     });
   });
+
+  describe('quando chamada a função create', () => {
+    const resultSetHeader = {
+      fieldCount: 0,
+      affectedRows: 1,
+      insertId: 1,
+      info: '',
+      serverStatus: 2,
+      warningStatus: 0
+    };
+
+    beforeAll(async () => {
+      sinon.stub(connection, 'execute').resolves([resultSetHeader])
+    });
+
+    afterAll(async () => {
+      connection.execute.restore();
+    });
+    
+    it('retorna um objeto ResultSetHeader contendo o insertId', async () => {
+      const result = await productsModel.create();
+      expect(result).to.be.an('object');
+      expect(result).to.have.property('insertId');
+    });
+  });
 });
