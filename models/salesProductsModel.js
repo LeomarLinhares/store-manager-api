f00e2d886b12a3c4b9013ab2c2bebed07a35e07b
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-09 10:44:56 -0300

    :recycle: refatora a estrutura do módulo salesProductsModel.js para que os testes de cobertura enxerguem as funções

--- models/salesProductsModel.js
+++ models/salesProductsModel.js
@@ -1,23 +1,28 @@
 const connection = require('./connection');
 const QUERY = require('../helpers/queries');
 
+const create = async (allSales) => {
+  const [rows] = await connection.query(QUERY.INSERT_SALES_PRODUCTS, [allSales]);
+  return rows;
+};
+
+const getAll = async () => {
+  const [rows] = await connection.execute(QUERY.SELECT_ALL_SALES_PRODUCTS);
+  return rows;
+};
+
+const getById = async (id) => {
+  const [rows] = await connection.execute(QUERY.SELECT_ALL_SALES_PRODUCTS_BY_ID, [id]);
+  return rows;
+};
+
+const update = async (productId, quantity, saleId) => {
+  await connection.execute(QUERY.UPDATE_SALES_PRODUCTS, [quantity, saleId, productId]);
+};
+
 module.exports = {
-  create: async (allSales) => {
-    const [rows] = await connection.query(QUERY.INSERT_SALES_PRODUCTS, [allSales]);
-    return rows;
-  },
-
-  getAll: async () => {
-    const [rows] = await connection.execute(QUERY.SELECT_ALL_SALES_PRODUCTS);
-    return rows;
-  },
-
-  getById: async (id) => {
-    const [rows] = await connection.execute(QUERY.SELECT_ALL_SALES_PRODUCTS_BY_ID, [id]);
-    return rows;
-  },
-
-  update: async (productId, quantity, saleId) => {
-    await connection.execute(QUERY.UPDATE_SALES_PRODUCTS, [quantity, saleId, productId]);
-  },
+  create,
+  getAll,
+  getById,
+  update,
 };
