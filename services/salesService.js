975c7358b41304b4e3b2b80487d73f8dd4841d4c
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-10 08:28:49 -0300

    :ok_hand: remove console.log

--- services/salesService.js
+++ services/salesService.js
@@ -15,7 +15,6 @@ module.exports = {
     try {
       const { insertId } = await salesModel.create();
       const allSales = sales.map((product) => [insertId, product.product_id, product.quantity]);
-      console.log(allSales);
       await salesProductsModel.create(allSales);
       return {
         id: insertId,
